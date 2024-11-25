c0e036b28889a006348d4c33232c25a98d5f4c4c
Ingunn Ofstad <ingofs00898@stud.noroff.no>
2024-11-25 10:15:41 +0100

    Commit 3: updated testing method and fixing a typo

--- Task_5_3.py
+++ Task_5_3.py
@@ -1,4 +1,13 @@
-from Task_5_3 import UserInputValidator
+from Task_5_1 import UserInputValidator
 
 instance_one = UserInputValidator()
 instance_two = UserInputValidator()
+
+test_one = ["Hello","World","!","1","-2","0"]
+test_two = ["3.4","2","1","0","-1"]
+
+result_one = instance_one.validate_positive_integers(test_one)
+print(result_one)
+
+result_two = instance_two.validate_positive_integers(test_two)
+print(result_two)
